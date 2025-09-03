63a8e21f578f430a76edea4b4da3fc6a0ff89f7b
moonyaejin <i0209i80@gmail.com>
2025-09-03 14:49:26 +0900

    feat: 좌석 조회를 레이어드로 분리 및 조회 API 추가

--- src/main/java/kr/hhplus/be/server/reservation/adapter/persistence/ConfirmedReservationRepository.java
+++ src/main/java/kr/hhplus/be/server/reservation/adapter/persistence/ConfirmedReservationRepository.java
@@ -1,10 +1,16 @@
 package kr.hhplus.be.server.reservation.adapter.persistence;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ConfirmedReservationRepository extends JpaRepository<ConfirmedReservationJpaEntity, Long> {
 
     boolean existsByConcertDateAndSeatNo(LocalDate concertDate, Integer seatNo);
+
+    @Query("select c.seatNo from ConfirmedReservationJpaEntity c where c.concertDate = :date")
+    List<Integer> findSeatNosByConcertDate(@Param("date") LocalDate date);
 }
