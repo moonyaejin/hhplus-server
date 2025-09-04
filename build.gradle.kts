78d55740c87faab89a3cd4d6403018bd948ba819
moonyaejin <i0209i80@gmail.com>
2025-09-04 15:44:44 +0900

    build : Spring Boot 3.4 세팅/의존성 추가 및 JPA/Redis 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,34 +32,30 @@ dependencyManagement {
 dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("org.springframework.boot:spring-boot-starter-validation")
-    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.apache.commons:commons-lang3:3.14.0")
 
-    runtimeOnly ("com.mysql:mysql-connector-j")
-    testImplementation ("org.springframework.boot:spring-boot-starter-test")
-
     // DB
-	runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.mysql:mysql-connector-j")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation ("org.mockito:mockito-junit-jupiter:5.11.0")
-    testImplementation ("org.assertj:assertj-core:3.26.0")
-
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+    testImplementation("org.assertj:assertj-core:3.26.0")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.3"))
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
-    testImplementation("org.testcontainers:redis")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    // Lombok
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
-
     testCompileOnly("org.projectlombok:lombok:1.18.34")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 }
